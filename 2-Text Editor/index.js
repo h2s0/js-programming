0c2ebee76eb8f232b576e013f01481c8b278a832
h2s0 <h2s0_@naver.com>
2024-02-16 00:55:00 +0900

    ⚡️ html 요소 선택

--- 2-Text Editor/index.js
+++ 2-Text Editor/index.js
@@ -1,10 +1,19 @@
+// 옵션 버튼 전체
 let optionsButtons = document.querySelectorAll(".option-button");
+// 1번째
+let formatButtons = document.querySelectorAll(".format");
+// 2번째
+let scriptButtons = document.querySelectorAll(".script");
+// 7번째꺼
+let linkButton = document.getElementById("createLink");
+// 9, 10, 11, 12번째
+let alignButtons = document.querySelectorAll(".align");
+// 13, 14 번째
+let spacingButtons = document.querySelectorAll(".spacing");
+// createlink 버튼 7번째, 폰트, 폰트크기 선택 내려오는거, 컬러선택칸 2개
 let advancedOptionButton = document.querySelectorAll(".adv-option-button");
 let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById("fontSize");
 let writingArea = document.getElementById("text-input");
-let linkButton = document.getElementById("createLink");
-let alignButtons = document.querySelectorAll(".align");
-let spacingButtons = document.querySelectorAll(".spacing");
-let formatButtons = document.querySelectorAll(".format");
-let scriptButtons = document.querySelectorAll(".script");
+
 
