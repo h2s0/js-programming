d8b8114e0e68bdacbb4b18da06d4b03e3d732897
h2s0 <h2s0_@naver.com>
2024-02-16 00:37:35 +0900

    🐛 getElementById 불러올 떄 . 제거

--- 2-Text Editor/index.js
+++ 2-Text Editor/index.js
@@ -1,9 +1,10 @@
 let optionsButtons = document.querySelectorAll(".option-button");
 let advancedOptionButton = document.querySelectorAll(".adv-option-button");
-let fontName = document.getElementById(".fontName");
-let writingArea = document.getElementById(".text-input");
-let linkButton = document.getElementById(".createLink");
+let fontName = document.getElementById("fontName");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
 let alignButtons = document.querySelectorAll(".align");
 let spacingButtons = document.querySelectorAll(".spacing");
 let formatButtons = document.querySelectorAll(".format");
 let scriptButtons = document.querySelectorAll(".script");
+
