1d1eaaf82393b1020cc1078aa7aa92d5997dc976
h2s0 <h2s0_@naver.com>
2024-02-24 17:30:24 +0900

    ✨ JS 작성 중

---
+++ 3-Pass Generator/index.js
@@ -0,0 +1,59 @@
+// password length 
+const lengthSlider = document.querySelector(".pass-length input");
+const options = document.querySelectorAll(".option input");
+const copyIcon = document. querySelector(".input-box span");
+const passwordInput = document.querySelector(".input-box input");
+const passIndicator = document.querySelector(".pass-indicator");
+const generateBtn = document.querySelector(".generate-btn");
+
+const characters = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    symbols: "!@#$%^&*(){}[],.|<>~-:;"
+}
+
+// 비밀번호 생성
+const generatePassword = () => {
+    // 비밀번호 생성에 사용될 가능한 모든 문자를 포함할 변수
+    let staticPassword = "",
+    // 생성될 랜덤 비밀번호를 저장할 변수
+    randomPassword ="",
+    // 중복 문자를 제외할지 나타내는 변수
+    excludeDuplicate = false,
+    // 슬라이더로 설정한 비밀번호 길이
+    passLength = lengthSlider.value;
+
+    // 체크 된 옵션에 따라 staticPassword 문자열 구성
+    options.forEach( option => {
+        // option 이 체크되어 있다면, option.id 를 보고 판단하여 설정
+        if(option.checked) {
+            // 소문자, 대문자, 숫자, 기호 중 하나 옵션일 때
+            if(option.id !== "exe-duplicate" && option.id !== "spaces") {
+                staticPassword += characters[option.id];
+            } else if(option.id === "spaces") {
+                staticPassword += `   ${staticPassword}   `;
+            } else {
+                excludeDuplicate = true;
+            }
+        }
+    });
+
+    // 설정된 비밀번호 길이만큼 반복하여 staticPassword에서 무작위 문자를 선택하고 randomPassword에 추가
+    for( let i = 0; i < passLength; i++ ) {
+        let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
+        if (excludeDuplicate) {
+            // 중복 문자 제외 옵션이 체크되어 있다면 이미 randomPassword 에 있는 문자는 추가하지 않음
+            !randomPassword.includes(randomChar) | randomChar == " " ? randomPassword += randomChar : i--;
+        } else {
+            randomPassword += randomChar;
+        }
+    }
+    // 생성된 randomPassword 를 입력 요소의 값으로 설정
+    passwordInput.value = randomPassword;
+}
+
+// 비밀번호 강도 표시
+const updatePassIndicator = () => {
+
+}
