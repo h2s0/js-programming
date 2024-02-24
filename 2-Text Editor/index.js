d88050f0d2ba7848e2d8ae64a054c9e7bed9fc22
h2s0 <h2s0_@naver.com>
2024-02-24 16:15:22 +0900

    ✨ js 코드 설명 추가

--- 2-Text Editor/index.js
+++ 2-Text Editor/index.js
@@ -16,6 +16,7 @@ let fontName = document.getElementById("fontName");
 let fontSizeRef = document.getElementById("fontSize");
 let writingArea = document.getElementById("text-input");
 
+// 사용 가능한 폰트 리스트
 let fontList = [
     "Arial",
     "Verdana",
@@ -26,7 +27,7 @@ let fontList = [
     "Cursive",
 ];
 
-// 초기 설정
+// 페이지가 로드될 때 실행될 초기 설정 함수
 const intializer = () => {
     // 버튼을 강조하는 highlighter 함수 호출
     highlighter(alignButtons, true);
@@ -55,13 +56,13 @@ const intializer = () => {
     fontSizeRef.value = 3;
 };
 
-// execCommand  : 웹 페이지의 편집 가능 영역에서 특정 명령을 실행하는 역할
+// execCommand  : 웹 페이지의 편집 가능 영역에서 특정 명령을 실행하는 함수
 // value : 명령에 대한 추가 정보
 const modifyText = (command, defaultUi, value) => {
     document.execCommand(command, defaultUi, value);
 };
 
-// 각 option button 에 이벤트 리스너 추가
+// 각 option button 에 클릭 이벤트 리스너 추가
 // 버튼을 클릭하면 해당 버튼의 id 를 command 로 사용하여 modifyText 함수 호출
 optionButtons.forEach((button) => {
     button.addEventListener("click", () => {
@@ -69,12 +70,16 @@ optionButtons.forEach((button) => {
     });
 });
 
+// 고급 옵션 버튼에 이벤트 리스너 추가
+// 선택된 옵션이 변경되면 modifyText 함수 호출
 advancedOptionButton.forEach((button) => {
     button.addEventListener("change", () => {
         modifyText(button.id, false, button.value);
     });
 });
 
+// 링크 생성 버튼에 클릭 이벤트 리스너 추가
+// 클릭하면 사용자에게 URL을 입력하라는 프롬프트가 표시되고, 입력한 URL로 링크가 생서됨
 linkButton.addEventListener("click", () => {
     let userLink = prompt("Enter a URL?");
     if(/http/i.test(userLink)){
@@ -85,6 +90,8 @@ linkButton.addEventListener("click", () => {
     }
 });
 
+// 주어진 클래스 이름을 가진 모든 버튼에 클릭 이벤트 리스너를 추가하는 함수
+// 클릭하면 'active' 클래스가 추가, 제거되어 강조 효과 적용
 const highlighter= (className, needsRemoval) => {
     className.forEach((button) => {
         button.addEventListener("click", () => {
@@ -104,10 +111,12 @@ const highlighter= (className, needsRemoval) => {
     });
 };
 
+// 지정된 클래스 이름을 가진 모든 버튼에서 'active' 클래스 제거
 const highlighterRemover = (className) => {
     className.forEach((button) => {
         button.classList.remove("active");
     });
 };
 
+// 페이지가 로드될 때 intializer 함수 실행
 window.onload = intializer();
